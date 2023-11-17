b453b8e3d45594acd7a8d61183e8029d7b470e2f
Paganigg <guilhermepagani27@gmail.com>
2023-11-17 12:04:04 -0300

    add models

---
+++ model/medico.py
@@ -0,0 +1,13 @@
+medico = {
+    "id": int(),
+    "nome": str(),
+    "cpf": str(),
+    "idade": int(),
+    "sexo": str(),
+    "telefone": str(),
+    "email": str(),
+    "cidade": str(),
+    "estado": str(),
+    "crm": str(),
+    "especialidade": str(),
+}
---
+++ model/paciente.py
@@ -0,0 +1,13 @@
+paciente = {
+    "id": int(),
+    "nome": str(),
+    "cpf": str(),
+    "idade": int(),
+    "sexo": str(),
+    "telefone": str(),
+    "email": str(),
+    "cidade": str(),
+    "estado": str(),
+    "diagnostico": str(),
+    "id_medico": int(),
+}
